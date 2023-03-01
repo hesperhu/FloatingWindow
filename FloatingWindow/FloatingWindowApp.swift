01b3b315ad5940eb696f0201445a9a2e3505600a
hesperhu <hesper.hu@gmail.com>
2023-03-02 06:02:11 +0800

    Initial Commit

---
+++ FloatingWindow/FloatingWindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingWindowApp.swift
+//  FloatingWindow
+//
+//  Created by hesper on 2023-03-02(Thu).
+//
+
+import SwiftUI
+
+@main
+struct FloatingWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
